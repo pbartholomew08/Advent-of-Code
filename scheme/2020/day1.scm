99089231fafcdcc84035704002a3c24e55d3e211
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2021-11-27 14:12:31 +0000

    Implement brute-force solution to 2020/day1 part 1

---
+++ scheme/2020/day1.scm
@@ -0,0 +1,49 @@
+;;; Advent of Code 2020/day1 Scheme implementation.
+;;; Copyright (C) 2021  Paul Bartholomew
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Day 1.
+;;; Given a list of numbers, find the pair whose sum is 2020 and return the product.
+;;;
+;;; As an example, given '(1721 979 366 299 675 1456) the pair '(1727 299) sum to 2020 and the
+;;; answer 1721*299=514579.
+
+(define (find-pair lst prop)
+  (define (run-search v n s)
+    (if (null? s)
+	(if (null? n)
+	    #f
+	    (run-search (car n) '() (cdr n)))
+	(if (prop v (car s))
+	    (cons v (car s))
+	    (run-search v (append n (list (car s))) (cdr s)))))
+  (run-search (car lst) '() (cdr lst)))
+
+(define (day1 lst)
+  (define (pair-product pair)
+    ;; Returns the product of a pair of numbers.
+    ;;
+    ;; @param pair : The pair whose product is sought.
+    ;; @returns    : The product.
+    (* (car pair) (cdr pair)))
+  (pair-product (find-pair lst
+			   (lambda (a b)
+			     (= (+ a b) 2020)))))
+
+(define (test-day1)
+  (define input '(1721 979 366 299 675 1456))
+  (define expect 514579)
+  (= (day1 input) expect))
