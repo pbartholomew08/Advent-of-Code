7464febaeb1e6b8979e4c020a0ae1e97bf410274
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2021-12-01 17:39:36 +0000

    Build utils.scm as a module
    Note this needs to be installed into
    
      /path/guile/searches/aoc/utils.go

--- scheme/utils.scm
+++ scheme/utils.scm
@@ -14,6 +14,10 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+(define-module (aoc utils)
+  #:export (read-file
+	    read-numbers-from-file))
+
 ;; Load required modules
 (use-modules (ice-9 rdelim))
 
