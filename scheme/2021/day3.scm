dee6d54a891a9aa81d497ef6ce0726df88c686b4
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2021-12-03 20:05:56 +0000

    2021/day3 part 1 solution

--- scheme/2021/day3.scm
+++ scheme/2021/day3.scm
@@ -23,3 +23,64 @@
 ;;;
 ;;; then GAMMA=10110=22 and EPSILON=01001=9.
 ;;; The power consumption is given by their product.
+
+(define example-input '(#b00100
+			#b11110
+			#b10110
+			#b10111
+			#b10101
+			#b01111
+			#b00111
+			#b11100
+			#b10000
+			#b11001
+			#b00010
+			#b01010))
+
+(define (count-even lst)
+  (fold + 0 (map (lambda (x)
+		   (if (even? x)
+		       1
+		       0))
+		 lst)))
+(define (majority-even? lst)
+  (>= (count-even lst)
+      (/ (length lst) 2)))
+
+(define (divide-list-by lst n)
+  (map (lambda (x)
+	 (floor (/ x n)))
+       lst))
+(define (compute-gamma lst)
+  (define (run-calculation g ctr n lst)
+    (if (= ctr n)
+	g
+	(run-calculation (if (majority-even? lst)
+			     g
+			     (+ g (expt 2 ctr)))
+			 (+ ctr 1)
+			 n
+			 (divide-list-by lst 2))))
+  (run-calculation 0 0 (string-length (number->string (list-max lst) 2)) lst))
+
+(define (compute-epsilon lst)
+  (define (init-epsilon e ctr n)
+    (if (= ctr n)
+	e
+	(init-epsilon (+ e (expt 2 ctr))
+		      (+ ctr 1)
+		      n)))
+  (- (init-epsilon 0 0 (string-length (number->string (list-max lst) 2)))
+     (compute-gamma lst)))
+
+(define (run-day3 lst)
+  (* (compute-gamma lst)
+     (compute-epsilon lst)))
+
+(define (list-max lst)
+  (define (find-max m lst)
+    (if (null? lst)
+	m
+	(find-max (max m (car lst))
+		  (cdr lst))))
+  (find-max (car lst) (cdr lst)))
