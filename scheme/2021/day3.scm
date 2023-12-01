5106190cb99b10095a3d4eaf1b4d526eb05a7638
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2023-12-01 08:01:40 +0000

    Code from 2021

--- scheme/2021/day3.scm
+++ scheme/2021/day3.scm
@@ -23,6 +23,11 @@
 ;;;
 ;;; then GAMMA=10110=22 and EPSILON=01001=9.
 ;;; The power consumption is given by their product.
+;;;
+;;; Part2
+;;;
+;;; To get the Oxygen and CO2 ratings, filter the input according to the most/least common
+;;; significant bit, respectively.
 
 (use-modules (srfi srfi-1))
 
@@ -39,14 +44,45 @@
 			#b00010
 			#b01010))
 
+(define (binary->string b)
+  (number->string b 2))
+(define (string->binary s)
+  (string->number s 2))
+(define (pad-binary-strings lst)
+  (let ((n (max-bit-length (map string->binary lst))))
+    (map (lambda (s)
+	   (string-pad s n #\0))
+	 lst)))
+
 (define (count-even lst)
   (fold + 0 (map (lambda (x)
-		   (if (even? x)
-		       1
-		       0))
+		   (bit-extract x 0 1))
 		 lst)))
+
+(define (string-char s n)
+  (define (pop-char l ctr)
+    (if (= ctr n)
+	(car l)
+	(pop-char (cdr l) (+ ctr 1))))
+  (pop-char (string->list s) 0))
+
+(define (find-common-bit lst)
+  (let ((n (length lst)))
+    (define (search b c lst)
+      (if (null? lst)
+	  c
+	  (search b
+		  (if (equal? (string-char (car lst) 0)
+			      b)
+		      (+ c 1)
+		      c)
+		  (cdr lst))))
+    (if (> (search "0" 0 lst) (/ n 2))
+	"0"
+	"1")))
+
 (define (majority-even? lst)
-  (>= (count-even lst)
+  (> (count-even lst)
       (/ (length lst) 2)))
 
 (define (divide-list-by lst n)
@@ -86,3 +122,34 @@
 	(find-max (max m (car lst))
 		  (cdr lst))))
   (find-max (car lst) (cdr lst)))
+
+(define (max-bit-length lst)
+  (define (search m lst)
+    (if (null? lst)
+	m
+	(search (max m (string-length (binary->string (car lst))))
+		(cdr lst))))
+  (search 0 lst))
+
+(define (compute-rating rating-predicate lst)
+  (define (run rating ctr lst)
+    (if (null? lst)
+	rating
+	(run (+ rating
+		(if (rating-predicate lst)
+		    (expt 2 ctr)
+		    0))
+	     (+ ctr 1)
+	     (divide-list-by (filter rating-predicate lst) 2))))
+  (let ((n (max-bit-length lst)))
+    (run 0 0 (map (lambda (b)
+		    (reverse-binary-number b n))
+		  lst))))
+
+(define (reverse-binary-number b nbits)
+  (let ((s (number->string b 2)))
+    (string->number (string-reverse (string-pad s
+						(max nbits
+						     (string-length s))
+						#\0))
+		    2)))
